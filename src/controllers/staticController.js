49da8ffe64da429aba61c180d2a656985a72c866
Sara <sarakang05@gmail.com>
2019-03-05 19:36:19 -0800

    topics

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,1 +1,5 @@
-res.render("static/index", {title: "Welcome to Bloccit"});
+module.exports = {
+  index(req, res, next){
+    res.render("static/index", {title: "Welcome to Bloccit"});
+  }
+}
---
+++ src/db/models/topic.js
@@ -0,0 +1,24 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Topic = sequelize.define('Topic', {
+    title: {
+    type: DataTypes.STRING,
+    allowNull: false
+},
+   description: {
+   type: DataTypes.STRING,
+   allowNull: false
+}
+  }, {});
+  Topic.associate = function(models) {
+    Topic.hasMany(models.Banner, {
+   foreignKey: "topicId",
+   as: "banners",
+ });
+ Topic.hasMany(models.Rule, {
+     foreignKey: "topicId",
+     as: "rules",
+   });
+  };
+  return Topic;
+};
